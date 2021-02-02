5ccfcebb6444791fb9285b2a83d8e0640024ccc1
JagadeeshanMS <jagadeeshanmsj@gmail.com>
2021-02-02 13:54:21 +0530

    added option to enter timetable

--- sh/automatic_class_joiner.sh
+++ sh/automatic_class_joiner.sh
@@ -4,6 +4,14 @@ hour=$(date +%H)
 minute=$(date +%M)
 day=$(date +%w)
 dir="/home/$USER/lib/"
+if [ "$day" = "6" ]
+then 
+{
+    echo  "What is your timetable?\n"
+    echo "1 for Monday\n2 for Tuesday\n3 for Wednesday\n4 for Thrusday\n5 for Friday\n"
+    read -p "Enter here(1-5):" day
+}
+fi
 cd
 cd $dir/c++ && g++ link_provider.cpp -o link_provider 
 link=$(./link_provider $hour $minute $day)
