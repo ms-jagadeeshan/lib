f98d7929275cdb711cacaca283da97fefdd74e38
JagadeeshanMS <jagadeeshanmsj@gmail.com>
2021-02-04 23:09:29 +0530

    added ee class

--- c++/link_provider.cpp
+++ c++/link_provider.cpp
@@ -42,7 +42,7 @@ public:
 
 int main(int argc, char *argv[])
 {
-    timings a[20] = {
+    timings a[21] = {
         {8, 55, 1, "https://meet.google.com/qzi-ywmf-cry?pli=1&authuser=1"}, //Calculus ,Monday
         {9, 55, 1, "https://meet.google.com/zic-wtjg-maw?pli=1&authuser=1"},//Electomagnetics ,Monday
         {10, 55, 1, "https://teams.microsoft.com/l/channel/19%3a3ce9fe07ff004a9683ef056bc016551e%40thread.tacv2/General?groupId=905dfe47-e342-4fb0-8cd5-d7f98addfc2b&tenantId=be5109ea-c534-473c-af60-2054e070a0ed"},//Programming team link
@@ -54,6 +54,7 @@ int main(int argc, char *argv[])
         {10, 55, 2, "https://meet.google.com/lookup/hcstbcvhfm"},//Electric circuits batch-1 link ,Edit it if you are batch-2
         {11, 55, 2, "https://teams.microsoft.com/l/meetup-join/19%3ad7c001250cf44a56a26ddf61651226ea%40thread.tacv2/1607239355272?context=%7b%22Tid%22%3a%22be5109ea-c534-473c-af60-2054e070a0ed%22%2c%22Oid%22%3a%2227bcf7a3-f6f6-495d-b151-aa7c00445f43%22%7d"},//Materials Tuesday
         {14, 55, 2, "https://meet.google.com/qbm-bxsc-dvg?pli=1&authuser=1"}, //Programming practice
+        {9, 55, 3, "https://meet.google.com/zic-wtjg-maw?pli=1&authuser=1"},
         {13, 55, 3, "https://meet.google.com/osi-mxea-jpc?pli=1&authuser=1"},//Calculus ,Wednesday
         {14, 55, 3, "https://teams.microsoft.com/l/channel/19%3a4d6698b0643c44718a6799735a228dbd%40thread.tacv2/General?groupId=8d9df39f-d753-4bcf-9394-693ccec2c426&tenantId=be5109ea-c534-473c-af60-2054e070a0ed"},//Engineering and Product design
         {8, 55, 4, "https://meet.google.com/lookup/hcstbcvhfm"},//Electric circuits batch-1 link ,Edit it if you are batch-2
