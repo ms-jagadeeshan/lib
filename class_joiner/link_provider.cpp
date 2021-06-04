44bf1eb636509c5be3e545d5ecea84ea711f47c2
Jagadeeshan <jagadeeshanmsj@gmail.com>
2021-06-04 16:07:06 +0530

    links added

--- class_joiner/link_provider.cpp
+++ class_joiner/link_provider.cpp
@@ -68,39 +68,38 @@ public:
 int main(int argc, char *argv[])
 {
     timings a[] = {
-        {8, 55, 1, "fill_link_here", "Differential_Equations"},
-        {9, 55, 1, "fill_link_here", "Physics_Elective"},
-        {10, 55, 1, "fill_link_here", "Data_structure_and_algorithm"},
-        {11, 55, 1, "fill_link_here", "Engineering_graphics"},
-        {13, 55, 1, "fill_link_here", "Discrete_Structures_for_Computer_Science"},
-        {14, 55, 1, "fill_link_here", "Engineering_graphics_practice"},
-        {15, 55, 1, "fill_link_here", "Engineering_graphics_practice"},
-        {16, 55, 1, "fill_link_here", "Engineering_graphics_practice"},
-        {17, 55, 1, "fill_link_here", "Engineering_graphics_practice"},
-        {8, 55, 2, "fill_link_here", "Physics_Elective"},
-        {9, 55, 2, "fill_link_here", "Data_structure_and_algorithm"},
-        {11, 55, 2, "fill_link_here", "Discrete_Structures_for_Computer_Science"},
-        {13, 55, 2, "fill_link_here", "Differential_Equations"},
-        {14, 55, 2, "fill_link_here", "Data_structure_and_algorithm_practice"},
-        {15, 55, 2, "fill_link_here", "Data_structure_and_algorithm_practice"},
-        {16, 55, 2, "fill_link_here", "Data_structure_and_algorithm_practice"},
-        {17, 55, 2, "fill_link_here", "Data_structure_and_algorithm_practice"},
-        {13, 55, 3, "fill_link_here", "Physics_Elective"},
-        {14, 55, 3, "fill_link_here", "Sociology_of_design"},
-        {15, 55, 3, "fill_link_here", "Sociology_of_design"},
-        {16, 55, 3, "fill_link_here", "Sociology_of_design"},
-        {10, 55, 4, "fill_link_here", "Discrete_Structures_for_Computer_Science"},
-        {11, 55, 4, "fill_link_here", "Engineering_graphics"},
-        {13, 55, 4, "fill_link_here", "Differential_Equations"},
-        {15, 55, 4, "fill_link_here", "Design_and_Manufacturing_Lab"},
-        {9, 55, 5, "fill_link_here", "Differential_Equations"},
-        {10, 55, 5, "fill_link_here", "Physics_Elective"},
-        {13, 55, 5, "fill_link_here", "Data_structure_and_algorithm"},
-        {14, 55, 5, "fill_link_here", "Discrete_Structures_for_Computer_Science"}};
+        {8, 55, 1, "https://meet.google.com/rkg-ewkv-sjj?pli=1&authuser=1", "Differential_Equations"},
+        {9, 55, 1, "https://meet.google.com/wfp-jjbb-psb?hs=224&pli=1&authuser=1", "Physics_of_Materials"},
+        {11, 55, 1, "https://meet.google.com/ixr-ojie-aam?pli=1&authuser=1", "Engineering_graphics"},
+        {13, 55, 1, "https://meet.google.com/ixr-ojie-aam?pli=1&authuser=1", "Engineering_graphics_practice"},
+        {14, 55, 1, "https://meet.google.com/ixr-ojie-aam?pli=1&authuser=1", "Engineering_graphics_practice"},
+        {15, 55, 1, "https://meet.google.com/ixr-ojie-aam?pli=1&authuser=1", "Engineering_graphics_practice"},
+        {16, 55, 1, "https://meet.google.com/ixr-ojie-aam?pli=1&authuser=1", "Engineering_graphics_practice"},
+        {8, 55, 2, "https://meet.google.com/wfp-jjbb-psb?hs=224&pli=1&authuser=1", "Physics_of_Materials"},
+        {9, 55, 2, "https://meet.google.com/gzk-vopj-zid?pli=1&authuser=1", "Data_structure_and_algorithm"},
+        {11, 55, 2, "https://meet.google.com/smw-yhdt-mww?pli=1&authuser=1", "Discrete_Structures_for_Computer_Science"},
+        {13, 55, 2, "https://meet.google.com/gzk-vopj-zid?pli=1&authuser=1", "Data_structure_and_algorithm_practice"},
+        {14, 55, 2, "https://meet.google.com/mjn-buif-fvr?pli=1&authuser=1", "Data_structure_and_algorithm_practice"},
+        {15, 55, 2, "https://meet.google.com/mjn-buif-fvr?pli=1&authuser=1", "Data_structure_and_algorithm_practice"},
+        {16, 55, 2, "https://meet.google.com/mjn-buif-fvr?pli=1&authuser=1", "Data_structure_and_algorithm_practice"},
+        {11, 55, 3, "https://meet.google.com/smw-yhdt-mww?pli=1&authuser=1", "Discrete_Structures_for_Computer_Science"},
+        {14, 55, 3, "https://teams.microsoft.com/l/meetup-join/19%3a03857cdf7c9b4e4e82a47c8021e1dc6d%40thread.tacv2/1617025637706?context=%7b%22Tid%22%3a%22be5109ea-c534-473c-af60-2054e070a0ed%22%2c%22Oid%22%3a%2222c26465-12a8-4caf-b070-1a5f7ca97e25%22%7d", "Sociology_of_design"},
+        {15, 55, 3, "https://teams.microsoft.com/l/meetup-join/19%3a03857cdf7c9b4e4e82a47c8021e1dc6d%40thread.tacv2/1617025637706?context=%7b%22Tid%22%3a%22be5109ea-c534-473c-af60-2054e070a0ed%22%2c%22Oid%22%3a%2222c26465-12a8-4caf-b070-1a5f7ca97e25%22%7d", "Sociology_of_design"},
+        {16, 55, 3, "https://teams.microsoft.com/l/meetup-join/19%3a03857cdf7c9b4e4e82a47c8021e1dc6d%40thread.tacv2/1617025637706?context=%7b%22Tid%22%3a%22be5109ea-c534-473c-af60-2054e070a0ed%22%2c%22Oid%22%3a%2222c26465-12a8-4caf-b070-1a5f7ca97e25%22%7d", "Sociology_of_design"},
+        {8, 55, 4, "https://meet.google.com/wfp-jjbb-psb?hs=224&pli=1&authuser=1", "Physics_of_Materials"},
+        {9, 55, 4, "https://meet.google.com/gzk-vopj-zid?pli=1&authuser=1", "Data_structure_and_algorithm"},
+        {10, 55, 4, "https://meet.google.com/smw-yhdt-mww?pli=1&authuser=1", "Discrete_Structures_for_Computer_Science"},
+        {11, 55, 4, "https://meet.google.com/rkg-ewkv-sjj?pli=1&authuser=1", "Differential_Equations"},
+        {13, 55, 4, "https://meet.google.com/ixr-ojie-aam?pli=1&authuser=1", "Engineering_graphics"},
+        {15, 55, 4, "https://meet.google.com/lookup/d5twd57nfl", "Design_and_Manufacturing_Lab"},
+        {9, 55, 5, "https://meet.google.com/rkg-ewkv-sjj?pli=1&authuser=1", "Differential_Equations"},
+        {10, 55, 5, "https://meet.google.com/wfp-jjbb-psb?hs=224&pli=1&authuser=1", "Physics_of_Materials"},
+        {13, 55, 5, "https://meet.google.com/gzk-vopj-zid?pli=1&authuser=1", "Data_structure_and_algorithm"},
+        {14, 55, 5, "https://meet.google.com/smw-yhdt-mww?pli=1&authuser=1", "Discrete_Structures_for_Computer_Science"}};
 
     //converting the string arguments to integer
     int day1 = argv[3][0] - '0';
-    int hour1 = (strlen(argv[1]) == 1) ? (argv[1][0] - '0') : ((argv[1][0] - '0') * 10) + (argv[1][1] - '0'); 
+    int hour1 = (strlen(argv[1]) == 1) ? (argv[1][0] - '0') : ((argv[1][0] - '0') * 10) + (argv[1][1] - '0');
     int minute1 = ((argv[2][0] - '0') * 10) + (argv[2][1] - '0');
     //a[i].search(hour1, minute1, day1);
     int no_of_rows = sizeof(a) / sizeof(a[0]);
