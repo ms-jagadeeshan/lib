c25d8eacc24daacba87f5d9cde737fc7695e88d5
Jagadeeshan <jagadeeshanmsj@gmail.com>
2021-09-30 09:21:39 +0530

    bug fixed

--- class-joiner-v2/cjdaemon.sh
+++ class-joiner-v2/cjdaemon.sh
@@ -7,11 +7,11 @@ sixty=54
 twenty=114
 if [ "$MINUTE" -lt "54" ]; then
     count=$((sixty - MINUTE))
-   # sleep "$count"m
+    sleep "$count"m
 
 else
     count=$((twenty - MINUTE))
-   # sleep "$count"m
+    sleep "$count"m
 fi
 while true; do
     #classjoiner
